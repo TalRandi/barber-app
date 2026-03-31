981632717ea5d04ec67b89d72c89e2306e72e953
TalRandi <talrandi001@gmail.com>
2026-03-31 15:30:08 +0300

    Debug: log env var presence at startup

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
+console.log("[STARTUP] DATABASE_URL defined:", !!process.env.DATABASE_URL);
+console.log("[STARTUP] NODE_ENV:", process.env.NODE_ENV);
+
 import app from "../backend/src/app";
 
 export default app;
